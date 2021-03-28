4a9b9845ca5f719efecebea733d230d667fcbacf
victoriaWindle <>
2021-03-28 13:54:46 -0500

    added Javascript

--- js/main.js
+++ js/main.js
@@ -1,15 +1,36 @@
 (() => {
 
+console.log ("js is linked")
+
 //variables
-	const = jukebox = document.querySelectorAll(".jukeboxSquare")
-				discs = document.querySelectorAll(".discs")
-				discHome1 = document.querySelectorAll(".discH1")
-				discHome2 =  documment.querySelectorAll(".discH2")
+	const jukebox = document.querySelectorAll(".jukebox")
+			discs = document.querySelectorAll(".discs")
 
-//functions
 
+function dragStart(event) {
+	console.log("dragged!")
+	//event.dataTransfer.setData("savedID", this.id);
+}
+
+
+function draggedOver(event) {
+	//event.preventdefault();
+	console.log("I've been dragged over D:")
+}
+
+function dropped(event) {
+	event.preventDefault();
+	console.log("droppppp")
+}
 
 //event handling
+discs.forEach(piece => piece.addEventListener("dragstart", dragStart));
+
+jukebox.forEach(zone => {
+	zone.addEventListener("dragover", draggedOver);
+	zone.addEventListener("drop", dropped);
+})
+
 
 
 })();
