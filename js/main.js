d66d6463601e5468e23f7ddb331a754f69c4638c
victoriaWindle <>
2021-04-15 19:33:05 -0500

    changed audio and added javascript

--- js/main.js
+++ js/main.js
@@ -3,36 +3,47 @@
 console.log ("js is linked")
 
 //variables
-	const jukebox = document.querySelectorAll(".jukebox")
-			discs = document.querySelectorAll(".discs")
+	const jukebox = document.querySelector("#jukebox"),
+			discs = document.querySelectorAll(".discs"),
+			stop = document.querySelector(".stop");
 
 
 function dragStart(event) {
 	console.log("dragged!")
-	event.dataTransfer.setData("savedID", this.id);
+	event.dataTransfer.setData("savedID", event.target.id);
 }
 
 
 function draggedOver(event) {
-	//event.preventdefault();
+	event.preventDefault();
 	console.log("I've been dragged over D:")
 }
 
 function dropped(event) {
 	event.preventDefault();
-	console.log("droppppp")
 	let targetID = event.dataTransfer.getData("savedID");
-	console.log("dropped")
+	console.log("dropped", targetID)
+
+	//play the right audio piece there
+	let audioSrc = `audio/${targetID}.mp3`;
+	audio.src = audioSrc;
+	audio.load();
+	audio.play();
+}
+
+function stopAudio(event) {
+	console.log("paused!")
+	audio.pause();
+	audio.currentTime = 0;
 }
 
 //event handling
 discs.forEach(piece => piece.addEventListener("dragstart", dragStart));
 
-jukebox.forEach(zone => {
-	zone.addEventListener("dragover", draggedOver);
-	zone.addEventListener("drop", dropped);
-})
 
+	jukebox.addEventListener("dragover", draggedOver);
+	jukebox.addEventListener("drop", dropped);
 
+stop.addEventListener("click", stopAudio);
 
 })();
